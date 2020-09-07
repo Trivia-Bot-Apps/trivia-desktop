75e2826efc9018857001d9534ec9af076c1923b5
Evan Gubarev <evan@evangubarev.com>
2020-09-06 23:18:21 -0700

    Added Files

---
+++ index.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+      height: 600,
+      width: 800, 
+      webPreferences: {
+      	nodeIntegration: true
+      },
+  });
+  // mainWindow.removeMenu();
+  mainWindow.loadURL('file://' + __dirname + '/logo.html');
+  sleep(3000).then(() => {
+    mainWindow.loadURL('file://' + __dirname + '/launcher.html');
+  })
+});
