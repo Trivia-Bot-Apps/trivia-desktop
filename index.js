b54c6573b7dd617765edae8c400ba021532a95f3
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-10-30 16:52:49 -0700

    Refactored index

--- index.js
+++ index.js
@@ -6,17 +6,9 @@ function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  protocol.registerHttpProtocol(PROTOCOL_PREFIX, (req, cb) => {
-    const fullUrl = formFullTodoUrl(req.url)
-    devToolsLog('full url to open ' + fullUrl)
-    mainWindow.loadURL(fullUrl)
-  })
-}
 
 
-app.on('ready', () => {
+app.on('ready', async function () {
   mainWindow = new BrowserWindow({
       height: 600,
       width: 800, 
@@ -25,8 +17,7 @@ app.on('ready', () => {
       },
   });
   mainWindow.removeMenu();
-  mainWindow.loadURL('file://' + __dirname + '/logo.html');
-  sleep(3000).then(() => {
-    mainWindow.loadURL('file://' + __dirname + '/launcher.html');
-  })
+  mainWindow.loadFile('logo.html');
+  await sleep(3000)
+  mainWindow.loadFile('launcher.html');
 });
