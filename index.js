1c1106010c8dfa6c3b2eeea4ea0c087ddc812684
Evan Gubarev <evan@evangubarev.com>
2020-10-31 17:02:25 -0700

    Added Minimum IPC [Not Functional]

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const {app, BrowserWindow, protocol} = require('electron');
+const {app, BrowserWindow, protocol, ipcMain} = require('electron');
+const fs = require('fs');
 const PROTOCOL_PREFIX = 'trivia'
 let mainWindow;
 
@@ -7,6 +8,23 @@ function sleep (time) {
 }
 
 
+function getpoints() {
+    let points_data
+    fs.readFile('trivia.data', (err, data) => {
+        points_data = JSON.parse(data);
+        console.log(points_data);
+    });
+    return points_data // return_thingy
+}
+
+ipcMain.handle('get-points', async (event, someArgument) => {
+    let return_value
+    console.log(return_value)
+    return_value = getpoints();
+    return_value = return_value['points'];
+    console.log(return_value+'!')
+    return return_value
+})
 
 app.on('ready', async function () {
   mainWindow = new BrowserWindow({
@@ -16,7 +34,7 @@ app.on('ready', async function () {
       	nodeIntegration: true
       },
   });
-  mainWindow.removeMenu();
+  // mainWindow.removeMenu();
   mainWindow.loadFile('static/logo.html');
   await sleep(3000)
   mainWindow.loadFile('static/launcher.html');
