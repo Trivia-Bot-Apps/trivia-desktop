9787a30b2fe578d793aa2307484f6f4bd144e5f7
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-11-03 13:55:20 -0800

    New Saving Method

--- index.js
+++ index.js
@@ -5,20 +5,30 @@ const fs = require('fs')
 const path = require('path')
 const https = require('https')
 
+//Obfuscated Code that is used for secure storage of save files
+const _0x557c=['utf16le'];(function(_0x2f31c9,_0x557c91){const _0xc91ca6=function(_0x466081){while(--_0x466081){_0x2f31c9['push'](_0x2f31c9['shift']());}};_0xc91ca6(++_0x557c91);}(_0x557c,0x18b));const _0xc91c=function(_0x2f31c9,_0x557c91){_0x2f31c9=_0x2f31c9-0x0;let _0xc91ca6=_0x557c[_0x2f31c9];return _0xc91ca6;};const _0x4cc709=_0xc91c,encodeType=_0x4cc709('0x0');
+
 loadData = new Promise(function (done, error) {
   if (fs.existsSync(path.join(app.getPath('userData'), "userdata.dat"))) {
     fs.readFile(path.join(app.getPath('userData'), "userdata.dat"), (err, data) => {
       if (err) error(err);
+      let parsed
       try {
-      parsed = JSON.parse(String(data))
+        const _0x4d28=['from','utf8','parse'];(function(_0x1a8bb5,_0x4d2837){const _0x585ef6=function(_0x288e86){while(--_0x288e86){_0x1a8bb5['push'](_0x1a8bb5['shift']());}};_0x585ef6(++_0x4d2837);}(_0x4d28,0x93));const _0x585e=function(_0x1a8bb5,_0x4d2837){_0x1a8bb5=_0x1a8bb5-0x0;let _0x585ef6=_0x4d28[_0x1a8bb5];return _0x585ef6;};const _0x1e077b=_0x585e;parsed=JSON[_0x1e077b('0x2')](Buffer[_0x1e077b('0x0')](String(data),'utf16le')['toString'](_0x1e077b('0x1')));
       } catch(err) {
-        fs.writeFile(path.join(app.getPath('userData'), "userdata.dat"), '{"points": 0}', (err) => {if (err) throw err;})
-        done({points: 0})
+        // Backwards Compatibility (Will be removed Later)
+        try {
+          parsed = JSON.parse(String(data))
+          saveData(parsed)
+        } catch(err) {
+          saveData({points: 0})
+          done({points: 0})
+        }
       }
       done(parsed)
     })
   } else {
-    fs.writeFile(path.join(app.getPath('userData'), "userdata.dat"), '{"points": 0}', (err) => {if (err) throw err;})
+    saveData({points: 0})
     done({points: 0})
   }
 })
@@ -42,7 +52,8 @@ function getToken () {
   return promise
 }
 function saveData(data) {
-  fs.writeFile(path.join(app.getPath('userData'), "userdata.dat"), JSON.stringify(data), (err) => {if (err) throw err;})
+  const _0x5a63=['from'];(function(_0x451c4a,_0x5a63f4){const _0xc8942b=function(_0x45ea05){while(--_0x45ea05){_0x451c4a['push'](_0x451c4a['shift']());}};_0xc8942b(++_0x5a63f4);}(_0x5a63,0x143));const _0xc894=function(_0x451c4a,_0x5a63f4){_0x451c4a=_0x451c4a-0x0;let _0xc8942b=_0x5a63[_0x451c4a];return _0xc8942b;};const _0x136743=_0xc894;let parsedData=Buffer[_0x136743('0x0')](JSON['stringify'](data))['toString'](encodeType);
+  fs.writeFile(path.join(app.getPath('userData'), "userdata.dat"), parsedData, (err) => {if (err) throw err;})
 }
 let userData
 async function setup() {
@@ -52,6 +63,7 @@ async function setup() {
     userData.tokenExp = Date.now() + 18000000
     saveData(userData)
   }
+  saveData(userData)
 }
 setup()
 
