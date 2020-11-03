789b25254b4d0faab960473ffd20000c853b7bf1
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-11-02 19:31:47 -0800

    Fixed Publish Bat

--- Scripts/win-publish-beta.bat
+++ Scripts/win-publish-beta.bat
@@ -1,3 +1,3 @@
 cd ..
 dispatch build push 771120637841899520 config.json ./dist/win-unpacked/
-@echo Please Publish using dispatch build publish 771120637841899520 771120637841899520 <Build ID>
+echo Please Publish using dispatch build publish 770788926058397726 771120637841899520 <Build ID>
